1d5783cf22ad1d5b224905201723c650e202d4df
sergey_gerasimov <cross9494@mail.ru>
2025-04-18 10:43:01 +0500

    update main

--- bot_v2/main.py
+++ bot_v2/main.py
@@ -1,7 +1,7 @@
 import re
 import requests
 
-from constants import TELEGRAM_USERS_URL, WISHES_URL
+from constants import TELEGRAM_USERS_URL  # WISHES_URL
 from text_constants import (
     HELLO_TEXT,
     REGISTRY_REPLY_TEXT,
@@ -11,43 +11,51 @@ from text_constants import (
     GET_HELP_INFO_TEXT,
 )
 from keyboards import (
-    main_keyboard,
+    delete_confirm_keyboard,
     manage_wishes_keyboard,
-    confirm_delete_keyboard,
-    update_parametrs_list_keyboard,
-    # confirm_update_keyboard,
+    main_keyboard,
+    update_parametr_keyboard,
+    # update_confirm_keyboard,
     wish_list_keyboard,
 )
 from utils import (
-    bot,
     add_wish,
+    bot,
+    # delete_message,
+    delete_wish,
+    edit_message,
+    # edit_wish,
+    edit_keyboard,
+    get_about_text,
+    get_friend_text,
+    get_friend_wishes_text,
+    get_owner_wishes_text,
+    get_help_text,
     get_wish,
-    get_wish_list,
-    get_friends_list,
-    get_about_info,
-    get_help_info,
-    get_result_wish_data,
+    get_wish_id,
+    get_wish_data,
+    get_wishes_ids,
     send_message,
 )
 
 
 MAIN_KEYBOARD_COMMANDS = {
-    GET_MY_WISH_LIST_TEXT: get_wish_list,
-    GET_FRIEND_WISH_LIST_TEXT: get_friends_list,
-    GET_ABOUT_INFO_TEXT: get_about_info,
-    GET_HELP_INFO_TEXT: get_help_info
+    GET_MY_WISH_LIST_TEXT: get_owner_wishes_text,
+    GET_FRIEND_WISH_LIST_TEXT: get_friend_text,
+    GET_ABOUT_INFO_TEXT: get_about_text,
+    GET_HELP_INFO_TEXT: get_help_text
 }
 
 
 # Обработка начала работы бота
 @bot.message_handler(commands=['start'])
-def start_bot(message):
+def bot_start(message):
     try:
         response = requests.get(
             f'{TELEGRAM_USERS_URL}{message.chat.id}/'
         )
         if response.status_code == 200:
-            send_message(message, REGISTRY_REPLY_TEXT)
+            send_message(message, REGISTRY_REPLY_TEXT, main_keyboard())
         else:
             response = requests.post(
                 TELEGRAM_USERS_URL,
@@ -57,7 +65,6 @@ def start_bot(message):
                 }
             )
             send_message(message, HELLO_TEXT, main_keyboard())
-
     except requests.exceptions.RequestException as error:
         print(f'Ошибка: {error}')
 
@@ -65,181 +72,128 @@ def start_bot(message):
 # Обработка текстовых сообщений от пользователя
 @bot.message_handler(content_types=['text'])
 def bot_management(message):
+
+    # Обработка команд с основной клавиатуры
     if message.text in MAIN_KEYBOARD_COMMANDS:
-        MAIN_KEYBOARD_COMMANDS[message.text](message)
-
-    elif message.text.startswith('@'):
-        response = requests.get(
-            WISHES_URL,
-            params={
-                'owner_username': message.text[1:]
-            }
-        )
-        wishes = response.json()
-
-        if len(wishes) == 0:
-            bot.send_message(
-                chat_id=message.chat.id,
-                text=f'У {message.text[1:]}, ещё нет желаний'
+        text = MAIN_KEYBOARD_COMMANDS[message.text](message)
+        if message.text == GET_MY_WISH_LIST_TEXT:
+            send_message(
+                message=message, text=text,
+                keyboard=manage_wishes_keyboard()
             )
         else:
-            result_message = ''
-            for wish in wishes:
-                wish = [
-                    item for item in get_result_wish_data(wish)
-                ]
-                result_message = '\n'.join(wish)
-                bot.send_message(
-                    chat_id=message.chat.id,
-                    text=result_message
-                )
+            send_message(message=message, text=text)
+
+    # Взаимодействие с друзьями
+    elif message.text.startswith('@'):
+        text = get_friend_wishes_text(message)
+        send_message(message=message, text=text)
+
+    # Если текст не команда и не username, то добавляем как желание
     else:
         add_wish(message)
+        send_message(
+            message, f'"{message.text.capitalize()}" добавлено в ваш WishList'
+        )
 
 
+# Получения списка желаний для удаления/редактирования
+@bot.callback_query_handler(
+    func=lambda callback: (
+        (callback.data == 'delete') or (callback.data == 'update')
+    )
+)
+def get_wish_list_keyboard(callback):
+    wishes_ids = get_wishes_ids(callback)
+    edit_keyboard(
+        callback=callback,
+        keyboard=wish_list_keyboard(callback, wishes_ids)
+    )
+
+
+# Функция возврата к списку желаний
 @bot.callback_query_handler(func=lambda callback: callback.data == 'back')
-def back_button_handker(callback):
-    bot.edit_message_reply_markup(
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        reply_markup=manage_wishes_keyboard()
+def back_button_handler(callback):
+    edit_keyboard(
+        callback=callback,
+        keyboard=manage_wishes_keyboard()
     )
 
 
-# @bot.callback_query_handler(
-#     func=lambda callback: callback.data == 'confirm_back'
-# )
-# def confirm_back_button_handler(callback):
-#     bot.edit_message_text(
-#         chat_id=callback.message.chat.id,
-#         message_id=callback.message.message_id,
-#         text=get_wish_list(callback.message),
-#         reply_markup=manage_wishes_keyboard()
-#     )
-
-
-@bot.callback_query_handler(func=lambda callback: callback.data == 'delete')
-def delete_wish_list_handler(callback):
-    response = requests.get(
-        WISHES_URL,
-        params={
-            'owner_id': callback.message.chat.id
-        }
-    )
-    wishes = response.json()
-    wish_list_ids = [wish['id'] for wish in wishes]
-    wish_list_ids.reverse()
-
-    bot.edit_message_reply_markup(
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        reply_markup=wish_list_keyboard(callback, wish_list_ids)
+# Функция отмены удаления/редактирования
+@bot.callback_query_handler(
+    func=lambda callback: callback.data == 'cancel'
+)
+def cancel_button_handler(callback):
+    text = get_owner_wishes_text(callback.message)
+    edit_message(
+        callback=callback, text=text, keyboard=manage_wishes_keyboard()
     )
 
 
+# Подтверждение удаления желания
 @bot.callback_query_handler(
     func=lambda callback: re.search(r'^delete\d+$', callback.data)
 )
-def confirm_delete_wish_handler(callback):
-    wish_id = ''.join(
-        [letter for letter in callback.data if letter.isdigit()]
+def delete_confirm_handler(callback):
+    wish_id = get_wish_id(callback=callback)
+    wish = get_wish(wish_id)
+    result_message = 'Удалить это желание?\n' + '\n'.join(
+        [item for item in get_wish_data(wish)]
     )
-
-    result_message = '\n'.join(
-        [item for item in get_result_wish_data(get_wish(wish_id))]
-    )
-
-    bot.edit_message_text(
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
+    edit_message(
+        callback=callback,
         text=result_message,
-        reply_markup=confirm_delete_keyboard(wish_id)
+        keyboard=delete_confirm_keyboard(wish_id)
     )
 
 
-# @bot.callback_query_handler(
-#     func=lambda callback: re.search(r'^delete_confirm\d+$', callback.data)
-# )
-# def delete_wish_handler(callback):
-#     wish_id = ''.join(
-#         [letter for letter in callback.data if letter.isdigit()]
-#     )
-#     requests.delete(
-#         f'{WISHES_URL}{wish_id}/'
-#     )
-#     bot.edit_message_text(
-#         chat_id=callback.message.chat.id,
-#         message_id=callback.message.message_id,
-#         text=get_wish_list(callback.message),
-#         reply_markup=manage_wishes_keyboard()
-#     )
-
-
-@bot.callback_query_handler(func=lambda callback: callback.data == 'update')
-def update_wish_list_handler(callback):
-    response = requests.get(
-        WISHES_URL,
-        params={
-            'owner_id': callback.message.chat.id
-        }
-    )
-    wishes = response.json()
-    wish_list_ids = [wish['id'] for wish in wishes]
-    wish_list_ids.reverse()
-
-    bot.edit_message_reply_markup(
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        reply_markup=wish_list_keyboard(callback, wish_list_ids)
+# Удаление желания
+@bot.callback_query_handler(
+    func=lambda callback: re.search(r'^delete_confirm\d+$', callback.data)
+)
+def delete_handler(callback):
+    wish_id = get_wish_id(callback=callback)
+    delete_wish(wish_id)
+    text = get_owner_wishes_text(callback.message)
+    edit_message(
+        callback=callback, text=text, keyboard=manage_wishes_keyboard()
     )
 
 
+# Выбор параметра для редактирования
 @bot.callback_query_handler(
     func=lambda callback: re.search(r'^update\d+$', callback.data)
 )
 def choose_parametr_update_handler(callback):
-    wish_id = ''.join(
-        [letter for letter in callback.data if letter.isdigit()]
-    )
-    result_message = '\n'.join(
-        [item for item in get_result_wish_data(get_wish(wish_id))])
+    wish_id = get_wish_id(callback=callback)
+    wish = get_wish(wish_id)
+    text = '\n'.join(
+        [item for item in get_wish_data(wish)])
+    edit_message(
+        callback=callback, text=text,
+        keyboard=update_parametr_keyboard(wish_id=wish_id)
 
-    bot.edit_message_text(
-        chat_id=callback.message.chat.id,
-        message_id=callback.message.message_id,
-        text=result_message,
-        reply_markup=update_parametrs_list_keyboard(wish_id)
     )
 
 
-@bot.callback_query_handler(
-    func=lambda callback: re.search(r'update\w{5,12}\d+$', callback.data)
-)
-def confirm_update_wish_handler(callback):
-    wish_id = ''.join(
-        [letter for letter in callback.data if letter.isdigit()]
-    )
+# @bot.callback_query_handler(
+#     func=lambda callback: re.search(r'update\w{5,12}\d+$', callback.data)
+# )
+# def confirm_update_wish_handler(callback):
+#     wish_id = ''.join(
+#         [letter for letter in callback.data if letter.isdigit()]
+#     )
+#     if callback.data.startswith('update_title'):
+#         msg = bot.send_message(
+#             chat_id=callback.message.chat.id,
+#             text=f"Введите новое название для {get_wish(wish_id)['title']}"
+#         )
+#         bot.register_next_step_handler(
+#             msg,
+#             edit_wish,
+#             wish_id=wish_id,
+#             update_parametr='title'
+#         )
 
-    if callback.data.startswith('update_title'):
-        msg = bot.send_message(
-            chat_id=callback.message.chat.id,
-            text=f"Введите новое название для {get_wish(wish_id)['title']}"
-        )
-        bot.register_next_step_handler(
-            msg,
-            edit_wish,
-            wish_id=wish_id,
-            update_parametr='title'
-        )
-
-
-def edit_wish(message, wish_id, update_parametr):
-    requests.patch(
-        f'{WISHES_URL}{wish_id}/',
-        json={
-            update_parametr: message.text
-        }
-    )
-
-
-bot.polling()
+bot.polling(timeout=None)
